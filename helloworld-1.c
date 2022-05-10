5f55b2b8f1161e4af43e01bff2aff3b77e8db7f6
ying.xue <ecswai@163.com>
2022-05-11 00:44:31 +0800

    OverView：
    BUG：
    
    Description：

--- helloworld-1.c
+++ helloworld-1.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
+
+int func()
+{
+    print("aaaa");
+    return 0;
+}
 int main()
 {
     /* 我的第一个 C 程序 */
+    func();
     printf("Hello, World!3");
     return 0;
 
 }
+
